383e6f65e0584d803eae8f19827c737cba06d469
john <ipolekhin@yandex.ru>
2021-06-29 15:06:16 +0300

    add browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
 
     module: {
         rules: [
             {
-                test: /\.s?css$/,
+                test: /\.(s[ac]|c])css$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     "css-loader",
